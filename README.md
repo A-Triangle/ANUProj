6c29044ac7bace31ae83360444745d73c0f6b85d
lachan <lachlan.w1701e@gmail.com>
2023-05-15 17:01:12 +1000

    First implimentation

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# ANUProj
- hey hey
+## A collection (?) of python for structural biochem 
+
+# proj1
+
+A simple program for pulling down all of the known interacting proteins from a target (specified as a command line argument with the file).
+Progam will fetch the aa sequence of each interacting protein and output a fasta file to be run through alphafold multimer. 
---
+++ proj1.py
@@ -0,0 +1,30 @@
+import sys
+import requests
+import csv
+
+#finding string preferred identifier for query proten
+ProteinMatch = requests.get(f'https://string-db.org/api/json/get_string_ids?identifiers={sys.argv[1]}').json()
+StringID=ProteinMatch[0]["stringId"]
+
+#finding query protein fasta
+InitialSeq=requests.get(f'https://rest.ensembl.org/sequence/id/{StringID.removeprefix("9606.")}?content-type=text/x-fasta;species=homo_sapiens;type=cds').text 
+
+#finding interacting proteins
+interacting = requests.get(f'https://string-db.org/api/json/network?identifiers={StringID}').json()
+
+#scrubbing output of repeat proteins
+output=[]
+for entry in interacting:
+    if entry['stringId_A'] not in output:
+        output.append(entry['stringId_A'])
+      
+for entry in output:
+    entry = entry.removeprefix('9606.')
+    FindSeq = requests.get(f'https://rest.ensembl.org/sequence/id/{entry}?content-type=text/x-fasta;species=homo_sapiens;type=cds').text 
+    
+    file = open(f'output/{entry}.fasta', 'w')
+    
+    file.write(InitialSeq)
+    file.write(FindSeq)
+    
+    file.close()
