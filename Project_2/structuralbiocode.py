a2b33d9dc6cc4dfb7da6124d76450c2d302e144f
Lachlan <lachlan.w1701e@gmail.com>
2023-05-19 17:51:47 +1000

    added funcitonallity to split proteins based on predicted domain

--- Project_2/structuralbiocode.py
+++ Project_2/structuralbiocode.py
@@ -4,20 +4,21 @@ import csv
 domains = []
 
 #generating a list of lists containing predicted clusters from pae_to_domains...
-with open('/home/acube/anuproj/ANUProj/Project_2/tristanic/pae_to_domains/clusters.csv') as f:
+with open('/home/acube/anuproj/ANUProj/Project_2/pae_to_domains/clusters.csv') as f:
         output = csv.reader(f)
         for line in output:
             domains.append(line)
 
+#opening structure file to split
 with open('AF-P21580-F1-model_v4.pdb') as f:
     structure = Bio.PDB.PDBParser().get_structure('a20',f)
 
-model = structure[0]
-for chain in model:
-    print(f'chain {chain}, chainid: {chain.id}')
-
-chain_A = model['A']
-for res in chain_A:
-    print(f'residue name {res.resname}, resid: {res.id[2]}')
-
-    
+#filtering clusers.csv file
+for domain in domains:
+    i = 0
+    while i < len(domain):
+         if domain[i] == '':
+            del domain[i:len(domain)]
+            break
+         i+=1
+    Bio.PDB.Dice.extract(structure, 'A', int(domain[0]), int(domain[-1]), f'output/{domain[0]}-{domain[-1]}_domain.PDB')
